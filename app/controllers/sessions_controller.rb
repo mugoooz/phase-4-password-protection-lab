44dd51dc7491456f7ba4fdada3539cfeb591339f
Eric Mugo <mugoericpg20@gmail.com>
2023-06-28 22:27:34 +0300

    session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
-
+    def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :ok
+        else
+          render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+      end
     
+      def destroy
+        session[:user_id] = nil
+        head :no_content
+    end
+
+
 end
