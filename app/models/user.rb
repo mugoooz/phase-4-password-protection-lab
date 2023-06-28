0acd196458d78edfe7c171ac8d18fd05ac578553
Eric Mugo <mugoericpg20@gmail.com>
2023-06-28 22:23:25 +0300

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, confirmation: true
+    validates :password_confirmation, presence: true
+    
+  
+    has_many :recipes
+
 end
