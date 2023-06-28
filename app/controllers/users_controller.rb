81c22b3710ee1d185bb75cf2e32a51f1ca4b16af
Eric Mugo <mugoericpg20@gmail.com>
2023-06-28 22:22:45 +0300

    i

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+    
+        if user.save
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+end
